4692ceba8a2666a23b4e979376e370a4fc2cee65
Felipe P <fpalmasaldivia@gmail.com>
2022-10-10 22:06:29 -0300

    se corrige App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./Views/Home";
+import Navbar from "./Views/Navbar"
+import Pokemons from "./Views/Pokemons";
+import Details from "./Views/Details";
+
+export default function App() {
+    return (
+
+        <div className="app-total" >
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/Pokemons" element={<Pokemons />} />
+                    <Route path="/Pokemons/:name" element={<Details />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
 }
-
-export default App;
